f7607fa17900e4fb0c5632e179bde8d4558d3e16
Ishadireza <mishadirezap@gmail.com>
2023-05-26 15:13:15 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// App.js
+import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import TodoStatusFilter from './components/TodoStatusFilter';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Todo List</h1>
+      <TodoForm />
+      <TodoStatusFilter />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
